902ce0d5f0bd049db44f2e4670a0a3d745939b8d
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2022-02-26 16:48:10 +0300

    remove unused git ignore

--- app/base/settings.py
+++ app/base/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS: tp.List[str] = os.environ.get('ALLOWED_HOSTS', default='').split(',')  # type: ignore
+ALLOWED_HOSTS: tp.List[str] = os.environ.get('ALLOWED_HOSTS').split(',')  # type: ignore
 
 
 # Application definition

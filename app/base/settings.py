d6cb12a4b3f99da94b4b65e3e885b4e3b1ead6d1
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2022-02-26 16:47:22 +0300

    add ignore type

--- app/base/settings.py
+++ app/base/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', default=True)  # type: ignore
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS: tp.List[str] = os.environ.get('ALLOWED_HOSTS', default='').split(',')  # type: ignore
 

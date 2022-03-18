9d074ba520260873fd274cad500e4259aa4793fa
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2022-03-18 12:15:00 +0300

    add urls

--- app/base/urls.py
+++ app/base/urls.py
@@ -1,14 +1,15 @@
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 
-from base import settings
+from api.auth.views import CustomTokenObtainPairView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('api/', include(('api.video.urls', 'video'), namespace='video')),
+    path('api/', include(('api.auth.urls', 'auth'), namespace='auth')),
+    path('token/', CustomTokenObtainPairView.as_view(), name='token'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()

f864db8cccd58e6b5b06d8582d6dbfed69abca9b
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2022-03-21 00:07:13 +0300

    update urls

--- app/base/urls.py
+++ app/base/urls.py
@@ -1,7 +1,10 @@
+from rest_framework_simplejwt.views import TokenRefreshView
+
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 
+from base.yasg import urlpatterns as doc_urls
 from api.auth.views import CustomTokenObtainPairView
 
 urlpatterns = [
@@ -9,7 +12,9 @@ urlpatterns = [
 
     path('api/', include(('api.video.urls', 'video'), namespace='video')),
     path('api/', include(('api.auth.urls', 'auth'), namespace='auth')),
-    path('token/', CustomTokenObtainPairView.as_view(), name='token'),
+    path('auth/token/', CustomTokenObtainPairView.as_view(), name='token'),
+    path('auth/token_refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += doc_urls

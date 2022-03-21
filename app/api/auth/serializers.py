7e2bf36fb7b16cc096c3bdbaf2b76adcb18d108c
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2022-03-21 14:58:48 +0300

    update views; add tests for account

--- app/api/auth/serializers.py
+++ app/api/auth/serializers.py
@@ -1,11 +1,13 @@
 import typing as tp
 
+from django.core.validators import validate_email
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth import get_user_model, password_validation
 
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
+from api.auth.validation import validate_username_field, validate_email_field, validate_password_field
 from base import settings
 
 
@@ -23,23 +25,15 @@ class RegisterUserSerializer(serializers.Serializer):
 
     @staticmethod
     def validate_username(username: str) -> str:
-        is_exist = get_user_model().objects.filter(username=username).exists()
-        if is_exist:
-            raise serializers.ValidationError({'User with this username already exist'})
-        return username
+        return validate_username_field(username=username)
 
     @staticmethod
     def validate_email(email: str) -> str:
-        is_exist = get_user_model().objects.filter(email=email).exists()
-        if is_exist:
-            raise serializers.ValidationError({'User with this email already exist'})
-        return email
+        return validate_email_field(email=email)
 
     @staticmethod
     def validate_password(password: str) -> str:
-        # django will raise exception if bad value
-        password_validation.validate_password(password=password)
-        return password
+        return validate_password_field(password=password)
 
     def validate(self, attrs: tp.Dict[str, tp.Any]) -> tp.Dict[str, tp.Any]:
         """
@@ -79,3 +73,89 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
         del data['access']
         del data['refresh']
         return data
+
+
+class AccountModelSerializer(serializers.ModelSerializer):
+    """
+    Model Serializer for model user
+    """
+
+    # @staticmethod
+    # def validate_email(email):
+    #     validate_email(value=email)
+    #     is_exist = get_user_model().objects.filter(email=email).exists()
+    #     if is_exist:
+    #         raise serializers.ValidationError({'User with this email already exist'})
+    #     return email
+    #
+    # @staticmethod
+    # def validate_password(password: str) -> str:
+    #     password_validation.validate_password(password=password)
+    #     return password
+    #
+    # @staticmethod
+    # def validate_username(username: str) -> str:
+    #     is_exist = get_user_model().objects.filter(username=username).exists()
+    #     if is_exist:
+    #         raise serializers.ValidationError({'User with this username already exist'})
+    #     return username
+
+    def validate(self, attrs: tp.Dict[str, tp.Any]) -> tp.Dict[str, tp.Any]:
+        """
+        Validate all fields
+        Args:
+            attrs: OrderedDict with values
+        Raise:
+            exception if invalid fields
+        Returns:
+            attrs
+        """
+
+        attrs['password'] = make_password(password=attrs['password'])
+        return attrs
+
+    class Meta:
+        model = get_user_model()
+        fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name', 'is_staff']
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'is_staff': {'read_only': True}
+        }
+
+
+class UpdateAccountSerializer(serializers.Serializer):
+    """
+    Serializer for updating account credentials(include email, password, username)
+    """
+
+    username = serializers.CharField(max_length=100, required=False)
+    password = serializers.CharField(max_length=500, required=False)
+    email = serializers.CharField(max_length=100, required=False)
+    first_name = serializers.CharField(max_length=100, required=False)
+    last_name = serializers.CharField(max_length=100, required=False)
+
+    @staticmethod
+    def validate_username(username: str) -> str:
+        return validate_username_field(username=username)
+
+    @staticmethod
+    def validate_email(email: str) -> str:
+        return validate_email_field(email=email)
+
+    @staticmethod
+    def validate_password(password: str) -> str:
+        return validate_password_field(password=password)
+
+    def validate(self, attrs: tp.Dict[str, tp.Any]) -> tp.Dict[str, tp.Any]:
+        """
+        Validate all fields
+        Args:
+            attrs: OrderedDict with values
+        Raise:
+            exception if invalid fields
+        Returns:
+            attrs
+        """
+
+        attrs['password'] = make_password(password=attrs['password'])
+        return attrs
--- app/api/auth/views.py
+++ app/api/auth/views.py
@@ -1,13 +1,24 @@
 import typing as tp
 
 from drf_yasg.utils import swagger_auto_schema
+
+from django.contrib.auth import get_user_model
+
 from rest_framework import status
+from rest_framework import mixins
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.viewsets import GenericViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-from api.auth.serializers import RegisterUserSerializer, CustomTokenObtainPairSerializer
+from api.auth.permissions import IsOwnerOrReadOnlyAccountPermission
+from api.auth.serializers import (
+    RegisterUserSerializer,
+    CustomTokenObtainPairSerializer,
+    AccountModelSerializer,
+    UpdateAccountSerializer,
+)
 from api.auth.services import create_new_user
 
 
@@ -31,3 +42,39 @@ class CustomTokenObtainPairView(TokenObtainPairView):
     """
 
     serializer_class = CustomTokenObtainPairSerializer
+
+
+class AccountModelViewSet(mixins.ListModelMixin,
+                          mixins.RetrieveModelMixin,
+                          mixins.DestroyModelMixin,
+                          mixins.UpdateModelMixin,
+                          GenericViewSet):
+    """
+    Model View set for account
+    """
+
+    queryset = get_user_model().objects.all()
+    serializer_class = AccountModelSerializer
+    permission_classes = (IsOwnerOrReadOnlyAccountPermission,)
+
+    def update(self, request: Request, *args: tp.Any, **kwargs: tp.Any) -> Response:
+        # check permissions using object(has_object_permission)
+        self.get_object()
+
+        request_data = request.data.copy()
+
+        # serializer does not provide verification of the presence of values already in the user fields
+        # if true they will be deleted
+        if request_data.get('username') is not None and request_data.get('username') == request.user.username:
+            del request_data['username']
+        if request_data.get('email') is not None and request_data.get('email') == request.user.email:
+            del request_data['email']
+
+        serializer = UpdateAccountSerializer(data=request_data)
+        serializer.is_valid(raise_exception=True)
+        serializer_data = serializer.data
+
+        # I did not put one one line in services
+        if serializer_data:
+            get_user_model().objects.filter(pk=kwargs['pk']).update(**serializer_data)
+        return Response(data=request.data, status=status.HTTP_200_OK)
